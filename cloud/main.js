44609690dcef1090d42e3571d0fbb0f2b0b7ffd3
Eduardo Raad <eduardo.raad@cerebrum.ec>
2013-10-26 10:11:24 -0500

    Beta 1

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,63 @@
+Parse.Cloud.define("generateGeoPoints", function(request, response) {
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query("Recinto");
+    query.skip(3050);
+    query.limit(200);
+    query.find({
+	success: function(results) {
+	    var updatedObjects = [];
+	    for (var i = 0; i < results.length; ++i) {
+		if (results[i].get("Y") != null) {
+		    var updatedObject;
+		    var point = new Parse.GeoPoint({latitude: results[i].get("Y"), longitude: results[i].get("X")});
+		    updatedObject = results[i].set("position", point);
+		    updatedObjects.push(updatedObject);
+		}
+	    }
+	    Parse.Object.saveAll(updatedObjects, {
+		success: function(savedObjects) {
+		    response.success(savedObjects);
+		},
+		error: function(error) {
+		    console.log(error);
+		    response.error(error);
+		}
+	    });
+	},
+	error: function() {
+	    response.error("User lookup failed");
+	}
+    });
+});
 
-// Use Parse.Cloud.define to define as many cloud functions as you want.
-// For example:
-Parse.Cloud.define("hello", function(request, response) {
-  response.success("Hello world!");
+Parse.Cloud.job("bgGenerateGeoPoints", function(request, status) {
+    Parse.Cloud.useMasterKey();
+    var query = new Parse.Query("Recinto");
+    query.skip(2000);
+    query.limit(1000);
+    query.find({
+	success: function(results) {
+	    var updatedObjects = [];
+	    for (var i = 0; i < results.length; ++i) {
+		if (results[i].get("Y") != null) {
+		    var updatedObject;
+		    var point = new Parse.GeoPoint({latitude: results[i].get("Y"), longitude: results[i].get("X")});
+		    updatedObject = results[i].set("position", point);
+		    updatedObjects.push(updatedObject);
+		}
+	    }
+	    Parse.Object.saveAll(updatedObjects, {
+		success: function(savedObjects) {
+		    status.success("Migration completed successfully.")
+		},
+		error: function(error) {
+		    console.log(error);
+		    status.error("ocurrio un error");
+		}
+	    });
+	},
+	error: function() {
+	    status.error("ocurrio un error");
+	}
+    });
 });
---
+++ public/js/map.js
@@ -0,0 +1,38 @@
+var setupMap = function() {
+    var map = L.map('map').setView([-2.143722, -79.864697], 12);
+
+    L.tileLayer('http://{s}.tile.cloudmade.com/2d5a61297d38433e8da952ea7cb1e0df/997/256/{z}/{x}/{y}.png', {
+	attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+	maxZoom: 12
+    }).addTo(map);
+    map.locate({setView: true});
+    map.on("locationfound", function(e) {
+	loadGeoPoints(map, {lat: e.latlng.lat, lng: e.latlng.lng});
+    });
+};
+
+var drawPoints = function() {
+    
+};
+
+var loadGeoPoints = function(map, userLocation) {
+    Parse.initialize("4RFKcCR5vsItVPUnebOZ49G58NUpy1YBs6x5CS5f", "05JFLW8ZpeooKUAgxjkXU53vKezLKga6BCKdYV90");
+    var point = new Parse.GeoPoint({latitude: userLocation.lat, longitude: userLocation.lng});
+    var RecintoObject = Parse.Object.extend("Recinto");
+    var query = new Parse.Query(RecintoObject);
+    query.near("position", point);
+    query.limit(100);
+    query.find({
+	success: function(placesObjects) {
+	    _.map(placesObjects, function(item) {
+		var marker = L.marker([item.get("position").latitude, item.get("position").longitude]).addTo(map);
+		marker.bindPopup("<h3>" + item.get("RECINTO") + "</h3><p>" + item.get("DIR_RECINT") + "</p><p>" + item.get("PARROQUIA") + ", " +
+				 item.get("CANTON") + ", " + item.get("PROVINCIA") + "</p>")
+	    });
+	}
+    });
+};
+
+$( document ).ready(function() {
+    setupMap();
+});
